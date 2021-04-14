df838431a0bfbfe4cb70f1f341aa2e5046a3a52a
duvanrizky <82518933+duvanrizky@users.noreply.github.com>
2021-04-14 20:54:49 +0700

    projek
    projek hello

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("======================================")
+print("   Biodata   ")
+print("======================================")
+
+nama = input(" nama: ")
+nim = input(" nim: ")
+prodi = input(" prodi: ")
+
+print("Halo, nama %s, nim %s dan prodi %s" % (nama, nim, prodi))
